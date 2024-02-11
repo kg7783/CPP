2bec713c6efc35aa2f7880579b24f25de3ce6478
kg7783 <gerhard.kaeb@web.de>
2024-02-11 19:45:14 +0100

    Added directories 'Classes' and 'CMake' with containing files

---
+++ CMake/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kg7783/VisualStudioCode/CPP/CMake
+# Build directory: /home/kg7783/VisualStudioCode/CPP/CMake/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

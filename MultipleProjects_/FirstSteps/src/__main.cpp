579117995789e88e07fa27c76fbf5b15236587e2
kg7783 <gerhard.kaeb@web.de>
2024-03-04 20:48:29 +0100

    Removed folder Classes, added folder MultipleProjects_ with HelloWorld1, HellowWorld2, FirstSteps with g++ and CMake config

---
+++ MultipleProjects_/FirstSteps/src/__main.cpp
@@ -0,0 +1,28 @@
+#include "_functions.h"
+
+
+int main()
+{
+//    manage_deque();
+ 
+    manage_threads();
+//    manage_pthreads();
+
+//    manage_format();
+
+//    manage_any();
+
+//    manage_time();
+
+//    manage_fstream();
+
+//    manage_concepts();
+
+//    manage_test();
+
+//    manage_ranges();
+
+//    manage_calc();
+
+    return 0;
+}
